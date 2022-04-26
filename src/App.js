fc970ea0a7da556469ade203a9a7678f3439d042
Isaias Rodrigo <lima.isaias@aluno.ifsp.edu.br>
2022-04-26 14:37:41 -0300

    atualizei

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import './App.css';
-import ApiRequest from './components/demo/ApiRequest';
+import AddImage from './estudo/addimage';
+import ExemploContexto from './estudo/ExemploContexto';
+import UsandoProps from './estudo/UsandoProps'
+import LangContext from './estudo/LangContext'
+import { useState } from 'react'
 
-function app() {
-  return(
-    <>
-    <h1>Clean</h1>
-    <ApiRequest />
-    </>
+function App() {
+
+  const lang = {
+    br: {
+      welcome: 'Bem-Vindo',
+      changeLanguage: 'Alterar Idioma',
+      portuguese: 'Português',
+      english: 'Inglês',
+      textIntroduction: ''
+    },
+    en: {
+      welcome: 'Welcome',
+      changeLanguage: 'Change Language',
+      portuguese: 'Portuguese',
+      english: 'English'
+    }
+  }
+
+ const currentLangState = useState('br');
+ const [currentLang, setCurrentLanguage] = currentLangState;
+
+ const toggleLanguage = () => {
+  const newLanguage = currentLang === 'br' ? 'en' : 'br'
+  setCurrentLanguage(newLanguage)
+ }
+
+  return (
+    <LangContext.Provider value={[
+        currentLang === 'br' ? lang.br : lang.en,
+        toggleLanguage,
+        setCurrentLanguage
+      ]}>
+      <div className="App">
+        <UsandoProps />
+        <AddImage nome="Renan"/>
+        <ExemploContexto/>
+      </div>
+    </LangContext.Provider>
   );
 }
-export default app;
+
+export default App;
