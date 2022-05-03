2b6e5ea5fbceb1c00036511897028e1cdb398307
ISAIAS <cg3004121@LAB117M18.IFSP>
2022-05-03 17:56:38 -0300

    Add Sample useRef,Add Update User defaultValue and OnChange,Upgrade,React 18.1.0

--- src/estudo/Menu.js
+++ src/estudo/Menu.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Menu = () => {
   return (
     <div>
         Menu: <Link to="/">Home</Link> 
-        <Link style={{margin: '0 0 0 10px'}} to="images">Images</Link> 
-        <Link style={{margin: '0 0 0 10px'}} to="api-integration">Api Integration</Link> 
-        <Link style={{margin: '0 0 0 10px'}} to="props">Props</Link> 
-        <Link style={{margin: '0 0 0 10px'}} to="children">Children</Link> 
-        <Link style={{margin: '0 0 0 10px'}} to="images/icon">icon</Link> 
+        <NavLink style={{margin: '0 0 0 10px'}} to="images">Images</NavLink> 
+        <NavLink style={{margin: '0 0 0 10px'}} to="api-integration">Api Integration</NavLink> 
+        <NavLink style={{margin: '0 0 0 10px'}} to="props">Props</NavLink> 
+        <NavLink style={{margin: '0 0 0 10px'}} to="children">Children</NavLink> 
+        <NavLink style={{margin: '0 0 0 10px'}} to="images/icon">Icon</NavLink> 
     </div>
 
   )
